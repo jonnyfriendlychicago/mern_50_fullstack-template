cb23d60a7a36e990175edd5fe354f74b00a55741
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-05-11 13:14:34 -0500

    Basic CSS added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
+// ! import external css 
+import 'bootstrap/dist/css/bootstrap.min.css';
 // ! import any/all local css 
 import './App.css';
 // ! import all components
 import EntFormCmp from './components/EntFormCmp';
+// ! import required react-bootstrap items 
+import {Card} from 'react-bootstrap';
+import React,  {useState} from 'react'; // added this line
 
 function App() {
   return (
-    <div className="App">
-      <EntFormCmp />
-    </div>
+    <>
+    <header>
+      <div className="header_content"> 
+          <div className="header_content_vert_left"> 
+              <a href="#" className="header_home_link" >
+                  <h1 >Sitename</h1>
+                  <h2>Feel the love.</h2>
+              </a>
+          </div>
+          <div className="header_content_vert_right" > 
+              <h2>firstName LastName</h2>
+              <p>(UserID: plcehldr)</p>
+              <div className="header_content_horiz_right">
+                  <a href="#">My Profile</a>
+                  <a href="#">Logout</a>
+              </div>
+          </div>
+      </div>
+    </header>
+    <main className="row_flex_center_top ">
+      <div className="App">
+        <EntFormCmp />
+      </div>
+
+    </main>
+    </>
   );
 }
 
