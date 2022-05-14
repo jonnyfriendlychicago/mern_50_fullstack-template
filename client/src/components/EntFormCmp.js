f7879fe7a9e9e4536e4d0e29bc451c49262f50c8
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-05-14 13:53:41 -0500

    added various bootstrap fixes plus containers and rows

--- client/src/components/EntFormCmp.js
+++ client/src/components/EntFormCmp.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios'; 
-import {Card, Form} from 'react-bootstrap'; 
+import {Container, Row, Card, Form} from 'react-bootstrap'; 
 
 const EntFormCmp = (props) => {
 
@@ -26,81 +26,69 @@ const EntFormCmp = (props) => {
     }
 
     return (
-        <>
-        <Card style = {{width: '50rem', padding: '1rem', border: "0.1rem solid grey",  marginBottom: "0.5rem"}} > 
-            <h1>Form Title Goes Here</h1>
-            <Form onSubmit = {handleSubmit}>
-                <Form.Group className="mb-3 bg-white" controlId="FormGroup_01">
-                    <Form.Label>stringFieldOne:</Form.Label>
-                    <Form.Control
-                        style = {{width: '300px', height: "25px"}}
-                        type = "textarea"
-                        // value={toDo}
-                        onChange ={(e) => stringFieldOneSetter(e.target.value)}
-                    /> 
-                </Form.Group>
+        <Container>
+            <Row>
+                <Card style = {{width: '50rem', padding: '1rem', border: "0.1rem solid grey",  marginBottom: "0.5rem"}} > 
+                    <h2>Form Title Goes Here</h2>
+                    <Form onSubmit = {handleSubmit}>
+                        <Form.Group className="mb-3 bg-white" controlId="FormGroup_01">
+                            <Form.Label>stringFieldOne:</Form.Label>
+                            <Form.Control
+                                style = {{width: '300px', height: "25px"}}
+                                type = "textarea"
+                                // value={toDo}
+                                onChange ={(e) => stringFieldOneSetter(e.target.value)}
+                            /> 
+                        </Form.Group>
 
-                <Form.Group className="mb-3 bg-white" controlId="FormGroup_02">
-                    <Form.Label>stringFieldTwo:</Form.Label>
-                    <Form.Control
-                        style = {{width: '300px', height: "25px"}}
-                        type = "textarea"
-                        // value={toDo}
-                        onChange ={(e) => stringFieldTwoSetter(e.target.value)}
-                    /> 
-                </Form.Group>
+                        <Form.Group className="mb-3 bg-white" controlId="FormGroup_02">
+                            <Form.Label>stringFieldTwo:</Form.Label>
+                            <Form.Control
+                                style = {{width: '300px', height: "25px"}}
+                                type = "textarea"
+                                // value={toDo}
+                                onChange ={(e) => stringFieldTwoSetter(e.target.value)}
+                            /> 
+                        </Form.Group>
 
-                <Form.Group className="mb-3 bg-white" controlId="FormGroup_03">
-                    <Form.Label>numberField:</Form.Label>
-                    <Form.Control
-                        style = {{width: '300px', height: "25px"}}
-                        type = "textarea"
-                        // value={toDo}
-                        onChange ={(e) => numberFieldSetter(e.target.value)}
-                    /> 
-                </Form.Group>
+                        <Form.Group className="mb-3 bg-white" controlId="FormGroup_03">
+                            <Form.Label>numberField:</Form.Label>
+                            <Form.Control
+                                style = {{width: '300px', height: "25px"}}
+                                type = "textarea"
+                                // value={toDo}
+                                onChange ={(e) => numberFieldSetter(e.target.value)}
+                            /> 
+                        </Form.Group>
 
-                {/* below is fine, but not updated yet wed 5/11 */}
+                        {/* below is fine, but not updated yet wed 5/11 */}
 
-                {/* <Form.Group className="mb-3 bg-white" controlId="FormGroup_02">
-                    <Form.Label>Work Area:</Form.Label>
-                    <Form.Select 
-                            style = {{width: '300px', height: '35px'}} 
-                            aria-label="Default select example"
-                            onChange={ workAreaHandle }  >
-                            <option selected></option>
-                            <option value="diet">Diet</option>
-                            <option value="fitWell">Fitness/Wellness</option>
-                            <option value="homeMaintenance">Home Maintenance</option>
-                            <option value="work">Work</option>
-                            <option value="kids">Kids</option>
-                            <option value="contEd">Cont'd Ed/Skills</option>
-                            <option value="social">Social</option>
-                            <option value="yourTime">Your Thing</option>
-                    </Form.Select>
-                </Form.Group> */}
+                        {/* <Form.Group className="mb-3 bg-white" controlId="FormGroup_02">
+                            <Form.Label>Work Area:</Form.Label>
+                            <Form.Select 
+                                    style = {{width: '300px', height: '35px'}} 
+                                    aria-label="Default select example"
+                                    onChange={ workAreaHandle }  >
+                                    <option selected></option>
+                                    <option value="diet">Diet</option>
+                                    <option value="fitWell">Fitness/Wellness</option>
+                                    <option value="homeMaintenance">Home Maintenance</option>
+                                    <option value="work">Work</option>
+                                    <option value="kids">Kids</option>
+                                    <option value="contEd">Cont'd Ed/Skills</option>
+                                    <option value="social">Social</option>
+                                    <option value="yourTime">Your Thing</option>
+                            </Form.Select>
+                        </Form.Group> */}
 
-                <Form.Group className="mb-3" controlId="ToDo03">
-                    <Form.Control style = {{width: "100px"}} className="btn btn-primary" type = "submit" value="Submit it!"/>
-                </Form.Group>
-            </Form> 
-        </Card>
+                        <Form.Group className="mb-3" controlId="ToDo03">
+                            <Form.Control style = {{width: "100px"}} className="btn btn-primary" type = "submit" value="Submit it!"/>
+                        </Form.Group>
+                    </Form> 
+                </Card>
+            </Row>
+        </Container> 
 
-        {/* below is functional, but superceded by above. wed 5/11 */}
-
-        {/* <form onSubmit={handleSubmit}>
-            <p>
-                <label>stringFieldOne:</label><br/>
-                <input type="text" onChange={(e) => stringFieldOneSetter(e.target.value)}/>
-            </p>
-            <p>
-                <label>stringFieldTwo:</label><br/>
-                <input type="text" onChange={(e) => stringFieldTwoSetter(e.target.value)}/>
-            </p>
-            <input type="submit" />
-
-        </form> */}
-        </>
     )
 
 }; 
--- client/src/components/EntListCmp.js
+++ client/src/components/EntListCmp.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'; 
 import axios from 'axios';
-import {Card} from 'react-bootstrap'; 
+import {Container, Row, Card} from 'react-bootstrap'; 
 
 const EntListCmp = (props) => {
     
@@ -18,21 +18,28 @@ const EntListCmp = (props) => {
     }, [])
     
     return (
-        <Card style = {{width: '50rem', padding: '1rem', border: "0.1rem solid grey",  marginBottom: "0.5rem"}} > 
-            {
-                entList.map((ent, index)=>{
-                return (
-                    <div key={index}>
-                        <p >{ent.stringFieldOne}</p>
-                        <p> {ent.stringFieldTwo}</p>
-                        <p> {ent.numberField}</p>
-                        <p> Additional fields to be added here.</p>
-                        <Link to={`/ents/${ent._id}`}>{ent.stringFieldOne} Details</Link>
-                    </div>
-                )
-                })
-            }
-        </Card>
+        <Container> 
+            <Row>
+                {/* <Card style = {{width: '50rem', padding: '1rem', border: "0.1rem solid grey",  marginBottom: "0.5rem" }}> */}
+                    <h2>Ents</h2>
+                    {
+                        entList.map((ent, index)=>{
+                        return (
+                            // <div key={index}>
+                            <Card style = {{width: '15rem', padding: '0.5rem', border: "0.1rem solid grey",  margin: "0.25rem"}} >
+                                <p >{ent.stringFieldOne}</p>
+                                <p> {ent.stringFieldTwo}</p>
+                                <p> {ent.numberField}</p>
+                                <p> Additional fields to be added here.</p>
+                                <Link to={`/ents/${ent._id}`}>{ent.stringFieldOne} Details</Link>
+                            </Card>
+                            // </div>
+                        )
+                        })
+                    }
+                {/* </Card> */}
+            </Row>
+        </Container>
     )
 }; 
 
